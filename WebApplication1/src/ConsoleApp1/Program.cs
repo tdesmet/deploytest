843e0470a3c395817976150efbc143bdba2b63fa
Tom De Smet <tdesmet@gmail.com>
2016-08-26 10:23:27 +0200

    Add console app to simulate long running operation during deployment

---
+++ WebApplication1/src/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Console app started");
+            Task.Delay(100000).Wait();
+            Console.WriteLine("Wait time over, exiting");
+        }
+    }
+}
