cb7d9173e9b5905a34f37745a981f9cbea9e8308
Tom De Smet <tdesmet@gmail.com>
2016-08-26 09:04:18 +0200

    Delay startup by 100 seconds

--- WebApplication1/src/WebApplication1/Startup.cs
+++ WebApplication1/src/WebApplication1/Startup.cs
@@ -38,6 +38,7 @@ namespace WebApplication1
             services.AddApplicationInsightsTelemetry(Configuration);
 
             services.AddMvc();
+            Task.Delay(100000).Wait();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
